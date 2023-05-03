cc043c1a1c6864c617a0f75fb7b4a2637f3b8adf
amine0110 <mokhtari.amine1996@gmail.com>
2023-05-03 23:51:59 +0200

    shorten URL using Bitlys API

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def shorten_url(api_key, long_url):
         raise Exception(f"Error shortening URL: {response.status_code}, {response.text}")
 
 if __name__ == "__main__":
-    api_key = 'bc3fdc563514a2e7a8652ad0a9a1e547671f6198'
+    api_key = 'Your-API-Key'
     long_url = 'https://www.youtube.com/watch?v=nK1r_9hPWuI&ab_channel=LittleSoul'
     
     try:
